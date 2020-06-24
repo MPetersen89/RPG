7258281f99c9e2a4395bc220c4fa92176b184f3c
Michael <michael_petersen_email@yahoo.com>
2020-06-24 10:15:00 -0700

    create successful failure of empty attribute test.
    Co-Authored-By: Micheal Hansen <micheal.hansen@gmail.com>

--- __tests__/RPG.test.js
+++ __tests__/RPG.test.js
@@ -1,9 +1,20 @@
 import { Character } from './../src/Character.js';
 
 describe('Character', () => {
-  
+  let character;
+
+  beforeEach(() => {
+    character = new Character();
+  });
+
   test('should create a character object for the player', () => {
-    let character = new Character();
     expect(character).toBeDefined();
+  });
+
+  test('should populate the character with empty attributes', () => {
+    expect(character.health).toBeNull();
+    expect(character.strength).toBeNull();
+    expect(character.intelligence).toBeNull();
+    expect(character.dexterity).toBeNull();
   });
 });
